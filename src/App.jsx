59b8bdfe76771b9cde21cb7b3e7f3c8d7fce7f16
rezaul118255 <rezaul118255@gmail.com>
2024-01-12 23:33:24 +0600

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1> this is  App.js </h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+const Footer = () => {
+    return (
+        <div>
+            <h1>this is Footer page</h1>
+        </div>
+    );
+};
+
+export default Footer;
